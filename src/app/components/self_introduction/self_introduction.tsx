4bad95460404f22af8b6eadf59d01eb357ba86c3
CaduLopes87 <edu.lopesbezerra@gmail.com>
2023-12-05 20:41:02 -0300

    add: creating self introduction section

---
+++ src/app/components/self_introduction/self_introduction.tsx
@@ -0,0 +1,43 @@
+import Image from 'next/image'
+import "./self_introduction.scss"
+import { LinkedinIcon } from '../icons/LinkedinIcon'
+import { GitIcon } from '../icons/GitIcon'
+
+export function SelfIntrodution () {
+    return (
+        <div className='introduction-container'>
+            <div>
+                <h1>
+                    <span>Prazer,</span>
+                    <span>Eduardo Lopes</span>
+                </h1>
+                <h3>Desenvolvedor Web</h3>
+            </div>
+            <div>
+                <Image
+                    src="/perfil_pic.jpg"
+                    alt="Eduardo perfil picture"
+                    width={200}
+                    height={200}
+                    priority
+                    />
+            </div>
+            <div className='contact-btns'>
+                <nav className="social-medias">
+                    <a href="https://www.linkedin.com/in/carlos-eduardo-lopes-bezerra/" target='_blank'>
+                        <LinkedinIcon/>
+                    </a>
+                    <a href="https://github.com/CaduLopes87" target='_blank'>
+                        <GitIcon/>
+                    </a>
+                </nav>
+                <div >
+                    <button className="btn email-btn">Entre em Contato</button>
+                </div>
+                <div >
+                    <button className="btn cv-btn">Baixe o CV</button>
+                </div>
+            </div>
+        </div>
+    )
+}
