e6a88b7b09b5444b5a14c362f02af200b8002643
CaduLopes87 <edu.lopesbezerra@gmail.com>
2023-12-05 20:58:35 -0300

    add: creating about me section

---
+++ src/app/components/about/about.tsx
@@ -0,0 +1,16 @@
+import "./about.scss"
+
+export function About () {
+    return (
+        <div className='about'>
+                <h2>SOBRE</h2>
+                <p> Sou um desenvolvedor Web com experiência
+                em React.js, JavaScript, HTML, CSS, SASS, 
+                NodeJS, Express e consumo de APIs.
+                    Iniciei minha carreira como desenvolvedor em 
+                junho de 2023, atuando principalmente como
+                freelancer.
+                </p>
+        </div>
+    )
+}
