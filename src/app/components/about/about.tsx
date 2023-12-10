b27b6bd63290c048c13b82b20360011564ec1b36
CaduLopes87 <edu.lopesbezerra@gmail.com>
2023-12-10 19:13:52 -0300

    add: desktop responsivity

--- src/app/components/about/about.tsx
+++ src/app/components/about/about.tsx
@@ -3,14 +3,17 @@ import "./about.scss"
 export function About () {
     return (
         <div className='about'>
-                <h2>SOBRE</h2>
-                <p> Sou um desenvolvedor Web com experiência
-                em React.js, JavaScript, HTML, CSS, SASS, 
-                NodeJS, Express e consumo de APIs.
-                    Iniciei minha carreira como desenvolvedor em 
-                junho de 2023, atuando principalmente como
-                freelancer.
-                </p>
+            <div className="title-line">
+                <h2 className="title">SOBRE</h2>
+                <div className="line"></div>
+            </div>
+            <p> Sou um desenvolvedor Web com experiência
+            em React.js, JavaScript, HTML, CSS, SASS, 
+            NodeJS, Express e consumo de APIs.
+                Iniciei minha carreira como desenvolvedor em 
+            junho de 2023, atuando principalmente como
+            freelancer.
+            </p>
         </div>
     )
 }
